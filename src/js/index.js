8fb1cadb580a4e23d6f5cb749e0f98b88f496437
Alejandro <al225vh@student.lnu.se>
2024-05-03 12:01:38 +0800

    Try to fix routing issues on deployment

--- src/js/index.js
+++ src/js/index.js
@@ -30,7 +30,7 @@ if ("Notification" in window) {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
-      const registration = await navigator.serviceWorker.register('./serviceworker.js')
+      const registration = await navigator.serviceWorker.register('/serviceworker.js')
 
       console.log('ServiceWorker: Registration successful with scope: ', registration.scope)
     } catch (error) {
