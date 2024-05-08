3f8db6ce8210d7d86c99ca1a6de66c81c0450839
Alejandro <al225vh@student.lnu.se>
2024-05-08 16:12:34 +0800

    Rename the name of the register form input attributes

--- src/js/components/movement-minder-login-register/movement-minder-login-register.js
+++ src/js/components/movement-minder-login-register/movement-minder-login-register.js
@@ -230,7 +230,7 @@ span {
       <form id="registerForm">
         <div id="userNameContainer">
           <label for="userNameInput"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#a6a6a6" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg></label>
-          <input type="text" id="userNameInput" name="userName" placeholder="Username" min="1" max="240" required>
+          <input type="text" id="userNameInput" name="username" placeholder="Username" min="1" max="240" required>
         </div>
         <div id="emailContainer">
           <label for="emailInput"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#a6a6a6" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg></label>
@@ -238,7 +238,7 @@ span {
         </div>
         <div id="passWordContainer">
           <label for="passWordInput"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#a6a6a6" d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg></label>
-          <input type="password" id="passWordInput" name="passWord" placeholder="Password" min="1" max="1440" required>
+          <input type="password" id="passWordInput" name="password" placeholder="Password" min="1" max="1440" required>
         </div>
         <button class="loginRegisterButton" id="registerButton" type="submit">Register</button>
       </form>
@@ -468,6 +468,8 @@ customElements.define('movement-minder-login-register',
       formData.forEach((value, key) => {
         jsonData[key] = value;
       });
+
+      console.log(await jsonData)
 
       // Make a POST request to the server API.
       try {
