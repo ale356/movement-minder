afd379fb1f78b07f1b9e35e1d40a944b4f440469
Alejandro <al225vh@student.lnu.se>
2024-05-08 17:23:19 +0800

    Change api end point to local back end when login

--- src/js/components/movement-minder-login-register/movement-minder-login-register.js
+++ src/js/components/movement-minder-login-register/movement-minder-login-register.js
@@ -214,11 +214,11 @@ span {
       <form id="loginForm">
         <div id="userNameContainer">
           <label for="userNameInput"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#a6a6a6" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg></label>
-          <input type="text" id="userNameInput" name="userName" placeholder="Username" min="1" max="240" required>
+          <input type="text" id="userNameInput" name="username" placeholder="Username" min="1" max="240" required>
         </div>
         <div id="passWordContainer">
           <label for="passWordInput"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="17.5" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#a6a6a6" d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg></label>
-          <input type="password" id="passWordInput" name="passWord" placeholder="Password" min="1" max="1440" required>
+          <input type="password" id="passWordInput" name="password" placeholder="Password" min="1" max="1440" required>
         </div>
         <button class="loginRegisterButton" id="loginButton" type="submit">Login</button>
       </form>
@@ -424,7 +424,7 @@ customElements.define('movement-minder-login-register',
 
       // Make a POST request to the server API.
       try {
-        const response = await fetch('your_api_endpoint_here', {
+        const response = await fetch('http://localhost:8080/api/v1/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
@@ -435,6 +435,7 @@ customElements.define('movement-minder-login-register',
         if (response.ok) {
           // Handle successful login.
           console.log('Login successful');
+          console.log(response)
           // Redirect or perform other actions.
           this.#loginErrorMessageContainer.setAttribute('hidden', '')
           this.#loginRegisterContainer.setAttribute('hidden', '')
